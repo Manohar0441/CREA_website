9d100426b32134fb5456fa6ba2f0fdce9958c123
Manohar0441 <109275164+Manohar0441@users.noreply.github.com>
2024-01-01 12:06:16 +0530

    Add files via upload

---
+++ Crea Website/formhandler copy 2.php
@@ -0,0 +1,27 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['comment'];
+
+
+$email_from = 'info@[my website].com';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name : $name.\n".
+              "User Email : $visitor_email.\n".
+              "Subject : $subject.\n ";
+             
+
+$to = 'mstar022220@gmail.com';
+
+$headers = "From : $email_from \r\n";
+
+$headers .="Reply-To : $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location : home.html");
+
+
+?>
